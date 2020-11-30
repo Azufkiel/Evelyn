4e8274a614de8f8aeed0c89a1f8e8369805fcc3e
Azufkiel <55968227+Azufkiel@users.noreply.github.com>
2020-11-30 15:27:39 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -34,14 +34,27 @@
                   Happy Birthday Evelyn<br>
                   We Wish You All The Best
                 </a>
-            </h2>
+                <br>
             <button class="btn btn-primary" OnClick="vid()">Click Here.. i put a song for you</button>
+            </h2>
             <div class="video">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/ib8DHcaTFag" 
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/ib8DHcaTFag?autoplay=1" 
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen></iframe>
                 <h3>This is just a simple web i made for you.. hope you like it :)</h3>
+            </div>
+            <div class="im">
+                <div><img src="img/ballon2f.png"></div>
+                <div><img src="img/ballon3.png"></div>
+                <div><img src="img/unnamed.png"></div>
+                <div><img src="img/cake2f.png"></div>
+                <div><img src="img/cake2f.png"></div>
+                <div><img src="img/ballon4.png"></div>
+                <div><img src="img/cake2f.png"></div>
+                <div><img src="img/ballon4.png"></div>
+                <div><img src="img/ballon2f.png"></div>
+                <div><img src="img/unnamed.png"></div>
             </div>
             <div class="box">
                 <div></div>
@@ -60,8 +73,12 @@
     <script>
       function vid(){
         var vids = document.getElementsByClassName("video");
-        console.log(vids);
+        var box = document.getElementsByClassName("box");
+        var img = document.getElementsByClassName("im");
+
         vids[0].style.display = "block";
+        box[0].style.display = "none";
+        img[0].className = "img";
       }
     </script>
 </html>
